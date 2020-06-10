f257b7cedcf6dc7ce99db201af70a9b6ec75859f
Rogier Lodewijks <rogier@lodewijks.org>
2020-06-10 21:40:47 +0200

    Add methods for creating, reading and deleting teams

--- lib/teamstuff/client.rb
+++ lib/teamstuff/client.rb
@@ -26,7 +26,7 @@ module Teamstuff
     end
 
     private def json_response err_class = RequestFailed
-      response = yield
+      response = yield http_client
 
       if response.status.success?
         JSON.parse response.to_s
@@ -54,20 +54,80 @@ module Teamstuff
       end
     end
 
+    def user_id
+      userinfo['id']
+    end
+
+    def get_member id = user_id
+      json_response { http_client.get "https://teamstuff.com/data/members/#{id}" }
+    end
+
+    def get_member_profiles id = user_id
+      json_response { http_client.get "https://teamstuff.com/data/members/#{id}/profiles/" }
+    end
+
     def get_context
-      context_url = 'https://coachstuff.com/data/context'
-      @context = json_response { http_client.get(context_url) }
+      @context = json_response do |client|
+        client.get 'https://coachstuff.com/data/context'
+      end
     end
 
     def get_events from = Time.now.at_beginning_of_week, to = Time.now.at_end_of_week
-      events_url = 'https://teamstuff.com/data/events'
-      params =
+      json_response do |client|
+        client.get 'https://teamstuff.com/data/events',
+                   params:
+                       {
+                           start_date: from.iso8601,
+                           end_date: to.iso8601
+                       }
+
+      end
+    end
+
+    def get_payments
+      json_response do |client|
+        client.get 'https://teamstuff.com/data/payment-items',
+                   params: {my_payments: true}
+      end
+    end
+
+    def get_current_duties
+      get_duties current: true
+    end
+
+    def get_duties params = {history: true}
+      json_response do |client|
+        client.get 'https://teamstuff.com/data/duties',
+                   params: params
+      end
+    end
+
+    def get_teams
+      json_response { |cl| cl.get 'https://teamstuff.com/data/teams' }
+          .fetch('teams', [])
+    end
+
+    def create_team team_name:, sport:, league:, invites: [], open_invite: true, kids_team: true
+
+      team_params =
           {
-              start_date: from.iso8601,
-              end_date: to.iso8601
+              name: team_name,
+              kids_team: kids_team,
+              open_invitation_enabled: open_invite,
+              sport_attributes: {name: sport},
+              league_attributes: {name: league},
+              team_invitations_attributes: invites
           }
 
-      json_response { http_client.get(events_url, params: params) }
+      json_response do |client|
+        client.post 'https://teamstuff.com/data/teams', json: {team: team_params}
+      end
+    end
+
+    def delete_team id:
+      http_client
+          .delete("https://teamstuff.com/data/teams/#{id}")
+          .status.success?
     end
 
   end
